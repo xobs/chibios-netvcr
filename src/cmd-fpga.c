ec9436b619075a39a33903e515b384bc2a953d1d
Sean Cross <xobs@kosagi.com>
2016-10-13 05:12:10 +0000

    cmd-fpga: add a command to manipulate the fpga
    Signed-off-by: Sean Cross <xobs@kosagi.com>

---
+++ src/cmd-fpga.c
@@ -0,0 +1,83 @@
+#include <string.h>
+
+#include "hal.h"
+
+#include "fpga.h"
+
+#include "shellcfg.h"
+#include "chprintf.h"
+
+void fpgaCommand(BaseSequentialStream *chp, int argc, char *argv[])
+{
+
+  if (argc <= 0) {
+    chprintf(chp, "Usage: fpga [verb]:"SHELL_NEWLINE_STR);
+    chprintf(chp, "    cycle        Power cycle the FPGA"SHELL_NEWLINE_STR);
+    chprintf(chp, "    reset        Put the FPGA into reset"SHELL_NEWLINE_STR);
+    chprintf(chp, "    unreset      Bring the FPGA out of reset"SHELL_NEWLINE_STR);
+    chprintf(chp, "    connect      Connect the FPGA to the SPINOR"SHELL_NEWLINE_STR);
+    chprintf(chp, "    disconnect   Disconnect the FPGA to the SPINOR"SHELL_NEWLINE_STR);
+    chprintf(chp, "    status       Show status of the FPGA"SHELL_NEWLINE_STR);
+    return;
+  }
+
+  if (!strcasecmp(argv[0], "cycle")) {
+    chprintf(chp, "Power-cycling FPGA: ");
+
+    if (fpgaDisconnect()) {
+      chprintf(chp, "Unable to disconnect"SHELL_NEWLINE_STR);
+      return;
+    }
+
+    /* Let the FPGA think about things for a while, in reset */
+    chThdSleepMilliseconds(1);
+
+    if (fpgaConnect()) {
+      chprintf(chp, "Unable to reconnect"SHELL_NEWLINE_STR);
+      return;
+    }
+
+    chprintf(chp, "Ok"SHELL_NEWLINE_STR);
+  }
+
+  else if (!strcasecmp(argv[0], "connect")) {
+    chprintf(chp, "Connecting FPGA: ");
+    if (fpgaConnect())
+      chprintf(chp, "Error"SHELL_NEWLINE_STR);
+    else
+      chprintf(chp, "Ok"SHELL_NEWLINE_STR);
+  }
+
+  else if (!strcasecmp(argv[0], "disconnect")) {
+    chprintf(chp, "Disconnecting FPGA: ");
+    if (fpgaDisconnect())
+      chprintf(chp, "Error"SHELL_NEWLINE_STR);
+    else
+      chprintf(chp, "Ok"SHELL_NEWLINE_STR);
+  }
+
+  else if (!strcasecmp(argv[0], "reset")) {
+    chprintf(chp, "Putting FPGA into reset: ");
+    if (fpgaReset())
+      chprintf(chp, "Error"SHELL_NEWLINE_STR);
+    else
+      chprintf(chp, "Ok"SHELL_NEWLINE_STR);
+  }
+
+  else if (!strcasecmp(argv[0], "unreset")) {
+    chprintf(chp, "Bringing FPGA out of reset: ");
+    if (fpgaUnreset())
+      chprintf(chp, "Error"SHELL_NEWLINE_STR);
+    else
+      chprintf(chp, "Ok"SHELL_NEWLINE_STR);
+  }
+
+  else if (!strcasecmp(argv[0], "status")) {
+    chprintf(chp, "FPGA programmed: %s"SHELL_NEWLINE_STR,
+                                  fpgaProgrammed()?"Yes":"No");
+  }
+
+  else {
+    chprintf(chp, "Unrecognized command: %s"SHELL_NEWLINE_STR, argv[0]);
+  }
+}
